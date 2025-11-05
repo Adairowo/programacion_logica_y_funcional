403f31d472d4639571257515fcdb429fd00cabe9
Adair <111542919+Adairowo@users.noreply.github.com>
2025-11-05 13:21:54 -0700

    Jija

---
+++ prolog/holamundo.pl
@@ -0,0 +1,16 @@
+valioso(oro).
+valioso(plata).
+le_gusta(juan,juli).
+tiene(juan,libro)
+tiene(juli,libro)
+%DIAGRAMA GENEALOGICO
+progenitor(clara,jose).
+progenitor(tomas,jose).
+progenitor(tomas,isabel).
+progenitor(jose,ana).
+progenitor(jose,patricia).
+progenitor(patricia,jaime).
+
+% PROGENITOR(X,ANA), PROGENITOR(Y,X). PROGENITOR(Y,Z)
+%Es ana tia de jaime?
+%Es ana tia de isabel
